b140391e663527ceea9792e1b6d0fef379dffa6a
nnguyen2496 <ntn3@williams.edu>
2017-01-04 16:39:48 -0500

    Add files via upload

--- MSCI.R
+++ MSCI.R
@@ -1,9 +1,13 @@
 #download CSV to user's desktop
-
+source_link <- "https://www.ishares.com/us/products/239695/ishares-
+  msci-usa-minimum-volatility-etf/1467271812596.ajax?fileType=csv&fileName=USMV_holdings&dataType=fund"
+url <- getwd()
+path <- paste(url, "temp.csv", sep = "/")
+download.file(source_link, destfile = path)
 
 
 # Create dataframe from the csv
-all_content = readLines("C:\\Users\\Nam Nguyen\\Desktop\\1.csv")
+all_content = readLines(path)
 
 #delete first 10 rows (not relevant)
 skip_ten = all_content[-c(1:10)]
